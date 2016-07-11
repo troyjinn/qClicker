27669bdcfb0c2ccbde2d89e68c6b265d135df9af
Troyjinn <troyjinn1@gmail.com>
2016-07-11 19:20:53 +1000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var qurans = 0;
+var cursors = 0;
+
+function qClick(number){
+    qurans = qurans + number;
+    document.getElementById("qurans").innerHTML = qurans;
+};
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
+    if(qurans >= cursorCost){
+        cursors = cursors + 1;
+        qurans = qurans - cursorCost;
+        document.getElementById('cursors').innerHTML = cursors;
+        document.getElementById('qurans').innerHTML = qurans;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));
+    document.getElementById('cursorCost').innerHTML = nextCost;
+};

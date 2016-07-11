06e8c09ac824aed49987f4eac31ef13fb16f4356
Troyjinn <troyjinn1@gmail.com>
2016-07-11 19:46:44 +1000

    added feels

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var qurans = 0;
 var cursors = 0;
 var keyboards = 0;
+var feels = 0;
 
 function qClick(number){
     qurans = qurans + number;
@@ -20,18 +21,31 @@ function buyCursor(){
 };
 
 function buyKeyboard(){
-    var keyboardCost = Math.floor(50 * Math.pow(2,keyboards));
+    var keyboardCost = Math.floor(50 * Math.pow(1.3,keyboards));
     if(qurans >= keyboardCost){
         keyboards = keyboards + 1;
         qurans = qurans - keyboardCost;
-        document.getElementById('keyboards').innerHTML = cursors;
+        document.getElementById('keyboards').innerHTML = keyboards;
         document.getElementById('qurans').innerHTML = qurans;
     };
-    var nextCost = Math.floor(50 * Math.pow(2,cursors));
+    var nextCost = Math.floor(50 * Math.pow(1.3,keyboards));
     document.getElementById('keyboardCost').innerHTML = nextCost;
+};
+
+function buyFeel(){
+    var feelCost = Math.floor(200 * Math.pow(1.5,keyboards));
+    if(qurans >= feelCost){
+        feels = feels + 1;
+        qurans = qurans - feelCost;
+        document.getElementById('keyboards').innerHTML = feels;
+        document.getElementById('qurans').innerHTML = qurans;
+    };
+    var nextCost = Math.floor(200 * Math.pow(1.5,feels));
+    document.getElementById('feelCost').innerHTML = nextCost;
 };
 
 window.setInterval(function(){
     qClick(cursors);
     qClick(keyboards * 2);
+    qClick(keyboards * 5);
 }, 1000);

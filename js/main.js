b8c6315f0b2c05c94aeff02c7fcbd8f4c5f66219
Troyjinn <troyjinn1@gmail.com>
2016-07-11 19:37:07 +1000

    added keyboards

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var qurans = 0;
 var cursors = 0;
+var keyboards = 0;
 
 function qClick(number){
     qurans = qurans + number;
@@ -18,6 +19,19 @@ function buyCursor(){
     document.getElementById('cursorCost').innerHTML = nextCost;
 };
 
+function buyKeyboard(){
+    var keyboardCost = Math.floor(50 * Math.pow(2,keyboards));
+    if(qurans >= keyboardCost){
+        keyboards = keyboards + 1;
+        qurans = qurans - keyboardCost;
+        document.getElementById('keyboards').innerHTML = cursors;
+        document.getElementById('qurans').innerHTML = qurans;
+    };
+    var nextCost = Math.floor(50 * Math.pow(2,cursors));
+    document.getElementById('keyboardCost').innerHTML = nextCost;
+};
+
 window.setInterval(function(){
     qClick(cursors);
+    qClick(keyboards * 2);
 }, 1000);

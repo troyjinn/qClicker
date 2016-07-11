94b46c7904e6f4b48ac30d1fed290be234927fc1
Troyjinn <troyjinn1@gmail.com>
2016-07-11 19:26:45 +1000

    "UI" changes + fixes

--- js/main.js
+++ js/main.js
@@ -17,3 +17,7 @@ function buyCursor(){
     var nextCost = Math.floor(10 * Math.pow(1.1,cursors));
     document.getElementById('cursorCost').innerHTML = nextCost;
 };
+
+window.setInterval(function(){
+    qClick(cursors);
+}, 1000);
